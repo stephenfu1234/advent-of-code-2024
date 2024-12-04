fd352c764d721a05ae4963e908f5eded59d06fbc
stephenfu1234 <stephenfu1@gmail.com>
2024-12-04 19:48:43 +0800

    Day 4 part 1

--- Day 4/part1.py
+++ Day 4/part1.py
@@ -11,12 +11,60 @@ def solve(filename):
     raw_input = read_file(filename)
 
     # read into a list of lists, split by new line and then by space using list comprension
-    data = [i.split(' ') for i in raw_input.split('\n')] #if i == to apply filtering
+    data = [list(i) for i in raw_input.split('\n')]
 
-    print(data)
+    # add border thickness of 3 to avoid needing to handle out of bounds exceptions during text scanning
+    add_border_to_puzzle(data)
+
+    # list of scan directions (north, north east, east etc)
+    directions = [(-1,0), (-1,1), (0,1), (1,1), (1,0), (1,-1), (0,-1), (-1,-1)]
+
+    # get the coordinates of each X
+    x_locations  = []
+    for row_idx, row in enumerate(data):
+        for col_idx, letter in enumerate(row):
+            if letter == 'X':
+                x_locations.append((row_idx, col_idx))
+
+    total = 0
+
+    # for each X, scan for XMAS in each direction
+    for row_idx, col_idx in x_locations:
+        for direction in directions:
+            total += is_xmas(data, row_idx, col_idx, direction[0], direction[1])
+
+    print(total)
+    
     
     end_time = int(time.time() * 1000)
     print(f'Time taken for {filename} (ms): {end_time - start_time}')
 
+def is_xmas(data, row_idx, col_idx, row_direction, col_direction):
+    text = ''
+    for i in range(4):
+        text += data[row_idx + (i * row_direction)][col_idx + (i * col_direction)] 
+   
+    # return 1 if any of the strings match forward or backwards XMAS
+    return any((text == 'XMAS', text == 'SAMX'))
+
+def add_border_to_puzzle(data):
+    # this should be cleaned up to remove repetition
+    # add left and right border
+    for row in data:
+        row.insert(0,'#')
+        row.insert(0,'#')
+        row.insert(0,'#')
+        row.append('#')
+        row.append('#')
+        row.append('#')
+
+    # add top border
+    data.insert(0, (len(data[0]) + 6) * ['#']) 
+
+    # add bottom border
+    data.append(data[0])
+    data.append(data[0])
+    data.append(data[0])
+
 solve('test.txt')
-#solve('input.txt')
+solve('input.txt')
